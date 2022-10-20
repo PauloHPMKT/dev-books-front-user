d6709ecaac4ee9e4164becf2514d7805a0148097
Paulo Sergio <pauloserg861@gmail.com>
2022-10-19 22:37:51 -0300

    realizando testes com axios

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,49 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
+  <div>
+    <form @submit.prevent="createNewUser">
+      <div>
+        <input type="text" v-model="user.name" placeholder="digite seu nome de usuario" />
+      </div>
+      <div>
+        <input type="text" v-model="user.email" placeholder="Digite seu e-mail" />
+      </div>
+      <div>
+        <input type="password" v-model="user.password" placeholder="digite sua senha">
+      </div>
+      <div>
+        <button type="submit">Cadastrar</button>
+      </div>
+    </form>
+  </div>
   <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script>
+import axios from 'axios'
+export default {
+  name: 'App',
+  data() {
+    return {
+      users: [],
+      user: {
+        name: '',
+        email: '',
+        password: '',
+      }
+    }
+  },
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+  methods: {
+    async createNewUser() {
+      await axios.post('http://localhost:3005/api/user/register', {
+        name: this.user.name,
+        email: this.user.email,
+        password: this.user.password, 
+      }).then(res => console.log(res)) 
+    }
   }
 }
-</style>
+</script>
+
+<style scoped></style>
