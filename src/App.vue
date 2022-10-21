edeb335d0c3fdc14ce6f9576eb4e93f5e6451a16
Paulo Sergio <pauloserg861@gmail.com>
2022-10-20 22:12:32 -0300

    modelando estrutura do App vue

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,11 @@
 
 <template>
-  <div>
-    <form @submit.prevent="createNewUser">
-      <div>
-        <input type="text" v-model="user.name" placeholder="digite seu nome de usuario" />
-      </div>
-      <div>
-        <input type="text" v-model="user.email" placeholder="Digite seu e-mail" />
-      </div>
-      <div>
-        <input type="password" v-model="user.password" placeholder="digite sua senha">
-      </div>
-      <div>
-        <button type="submit">Cadastrar</button>
-      </div>
-    </form>
-  </div>
   <RouterView />
 </template>
 
 <script>
-import axios from 'axios'
 export default {
   name: 'App',
-  data() {
-    return {
-      users: [],
-      user: {
-        name: '',
-        email: '',
-        password: '',
-      }
-    }
-  },
-
-  methods: {
-    async createNewUser() {
-      await axios.post('http://localhost:3005/api/user/register', {
-        name: this.user.name,
-        email: this.user.email,
-        password: this.user.password, 
-      }).then(res => console.log(res)) 
-    }
-  }
 }
 </script>
 
