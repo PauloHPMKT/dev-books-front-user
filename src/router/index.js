1a2dfb565ffe9811b0facce8b7a807199fa4ddfd
Paulo Sergio <pauloserg861@gmail.com>
2022-10-20 22:13:10 -0300

    criando rota de login

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,14 +6,18 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: () => import('@/components/Layout/DefaultLayout.vue'),
+      children: [
+        {
+          path: '/login',
+          name: 'Login',
+          component: () => import('@/views/Login.vue')
+        }
+      ]
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     }
   ]
