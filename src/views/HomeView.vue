3dda2fa2b4515bab6f2c03d8b3de94593309d4e8
Paulo Sergio <pauloserg861@gmail.com>
2022-10-20 23:34:17 -0300

    criando sessoes da home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,19 @@
 <template>
-  <h1>Home Page</h1>
+  <div class="banner-home">
+    <h1>Home Page</h1>
+  </div>
+  <section>
+    Ultimos Livros Adicionados
+  </section>
+  <section>
+    Em alta
+  </section>
+  <div>
+    editoras
+  </div>
+  <div>
+    Destaques
+  </div>
 </template>
 
 <script>
