6f0883560d58e5d24b31c2a5ab9d7220854bed40
Paulo Sergio <pauloserg861@gmail.com>
2022-10-20 22:13:31 -0300

    criando view de login

---
+++ src/views/Login.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <form @submit.prevent="createNewUser">
+      <div>
+        <input type="text" v-model="user.name" placeholder="digite seu nome de usuario" />
+      </div>
+      <div>
+        <input type="text" v-model="user.email" placeholder="Digite seu e-mail" />
+      </div>
+      <div>
+        <input type="password" v-model="user.password" placeholder="digite sua senha">
+      </div>
+      <div>
+        <button type="submit">Cadastrar</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Login',
+  data() {
+    return {
+      users: [],
+      user: {
+        name: '',
+        email: '',
+        password: '',
+      }
+    }
+  },
+
+  methods: {
+    async createNewUser() {
+      await axios.post('http://localhost:3005/api/user/register', {
+        name: this.user.name,
+        email: this.user.email,
+        password: this.user.password, 
+      }).then(res => console.log(res)) 
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
