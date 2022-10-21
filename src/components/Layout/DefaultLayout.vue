bc2f8a30b9465f0cb4f716a2fc0490b819a14c0f
Paulo Sergio <pauloserg861@gmail.com>
2022-10-20 22:12:54 -0300

    criando template padrao

---
+++ src/components/Layout/DefaultLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    header da pagina
+    <main class="container-page">
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
+
+<style lang="scss" scoped></style>
