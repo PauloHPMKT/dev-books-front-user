213dd844469b43767b990edb0e482995ae20ee11
Paulo Sergio <pauloserg861@gmail.com>
2022-10-20 22:44:52 -0300

    criando header da pagina

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="header">
+    <router-link to="/">
+      <img src="/img/livraria-do-dev-logo.png" alt="Logo DevBooks">
+    </router-link>
+    navbar
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style lang="scss" scoped></style>
