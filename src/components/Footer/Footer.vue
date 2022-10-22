4c681e0c52591b2a9f28aa18a7da58b12e6ec64b
Paulo Sergio <pauloserg861@gmail.com>
2022-10-22 20:01:01 -0300

    concluindo estilizacao da pagina de login

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,7 +1,14 @@
 <template>
   <footer>
     <div class="footer">
-      Footer da pagina
+      <img src="img/livraria-do-dev-logo.png" alt="Logo DevBooks">
+      <div>
+        <p>
+          Somos uma rede social criada especialmente para quem ama uma boa leitura.
+          Se você também ama enriquecer sua cultura com uma boa leitura e ainda contribuir
+          para que outras pessoas também se apaixonem por livro, você veio ao lugar certo. Junte-se nós e compartilhe experiências literárias.
+        </p>
+      </div>
     </div>
     <div class="footer-info">
       Desenvolvido por HP Design e Web
@@ -10,9 +17,7 @@
 </template>
 
 <script>
-import NavBar from '../Header/NavBar.vue'
 export default {
-  components: { NavBar },
   name: 'Footer',
 }
 </script>
