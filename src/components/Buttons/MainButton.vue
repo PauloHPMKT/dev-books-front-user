23a9e7c4a9a2bc92884151d7c498c556527a01f0
Paulo Sergio <pauloserg861@gmail.com>
2022-10-22 11:40:38 -0300

    criando componente de botao

---
+++ src/components/Buttons/MainButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button>{{ data_title }}</button>
+</template>
+
+<script>
+export default {
+  name: 'MainButton',
+  props: {
+    data_title: String,
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
