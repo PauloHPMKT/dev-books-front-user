6f4cf36954979dded50eeb89e358dc9faa3676c9
Paulo Sergio <pauloserg861@gmail.com>
2022-10-22 11:41:03 -0300

    criando aside e implementando estilos

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,16 @@
   <div class="banner-home">
     <h1>Olá, vamos enriquecer nossa cultura hoje?</h1>
   </div>
+  <aside class="aside-container">
+    <!-- implementar randomico com a foto dos usuários da plataforma -->
+    <div>
+      <h3>Junte-se a nós e ajude nossa comunidade a crescer.</h3>
+      <p>Este é o lugar certo para quem ama uma boa leitura, adora opinar sobre as obras, ama enriquecer sua cultura e gosta de compartilhar com seus amigos. <span>Aqui você encontra os melhores livros, editoras e autores.</span></p>
+    </div>
+    <div>
+      <MainButton :data_title="btn_title"/>
+    </div>
+  </aside>
   <section>
     Ultimos Livros Adicionados
   </section>
@@ -17,8 +27,16 @@
 </template>
 
 <script>
+import MainButton from '../components/Buttons/MainButton.vue'
+
 export default {
+  components: { MainButton },
   name: 'HomeView',
+  data() {
+    return {
+      btn_title: 'Cadastre-se, É de graça!'
+    }
+  }
 }
 </script>
 
