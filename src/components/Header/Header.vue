0a949fcab2e912e210a88d0a9f6cf645df8209fd
Paulo Sergio <pauloserg861@gmail.com>
2022-10-22 13:20:19 -0300

    criando navbar

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -3,13 +3,23 @@
     <router-link to="/">
       <img src="/img/livraria-do-dev-logo.png" alt="Logo DevBooks">
     </router-link>
-    navbar
+    <NavBar :routers="this.router_list.name"/>
   </header>
 </template>
 
 <script>
+import NavBar from '@/components/Header/NavBar.vue'
+
 export default {
   name: 'Header',
+  components: { NavBar },
+  data() {
+    return {
+      router_list: {
+        name: ['Livros', 'Editora', 'Autores', 'Adicionados', 'Topmais', 'Encontre outros usuários', 'Login'],
+      }
+    }
+  }
 }
 </script>
 
---
+++ src/components/Header/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav>
+    <ul class="menu-navigation">
+      <li>
+        <router-link v-for="router in routers"  :key="router" :to="`/${ router }`">
+          {{ router }}
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  props: {
+    routers: Array,
+  },
+}
+</script>
+
+<style lang="sass" scoped></style>
