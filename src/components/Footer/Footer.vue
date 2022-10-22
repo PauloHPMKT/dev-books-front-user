641d907d7beb71dc8497685231a75ebeb8873de4
Paulo Sergio <pauloserg861@gmail.com>
2022-10-22 19:21:09 -0300

    criando componente de Footer

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>
+    <div class="footer">
+      Footer da pagina
+    </div>
+    <div class="footer-info">
+      Desenvolvido por HP Design e Web
+    </div>
+  </footer>
+</template>
+
+<script>
+import NavBar from '../Header/NavBar.vue'
+export default {
+  components: { NavBar },
+  name: 'Footer',
+}
+</script>
+
+<style lang="scss" scoped></style>
