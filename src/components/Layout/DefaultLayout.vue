79d4c3fd4512eae30fae55a59549d4875148d830
Paulo Sergio <pauloserg861@gmail.com>
2022-10-22 19:20:43 -0300

    implementando Foorter ao template

--- src/components/Layout/DefaultLayout.vue
+++ src/components/Layout/DefaultLayout.vue
@@ -4,13 +4,15 @@
     <main class="container-page">
       <router-view />
     </main>
+    <Footer />
   </div>
 </template>
 
 <script>
+import Footer from '../Footer/Footer.vue'
 import Header from '../Header/Header.vue'
 export default {
-  components: { Header },
+  components: { Header, Footer },
   name: 'DefaultLayout',
 }
 </script>
